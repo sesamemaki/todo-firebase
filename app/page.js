a8f61ff488d11ede6a44c4c3dbb6b3612da3ae72
Burcu Bayraö <burju7@yahoo.com>
2023-06-22 21:12:44 +0300

    worked on vercel deployment issues

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import { AiOutlinePlus } from "react-icons/ai";
 import { db } from "../app/src/firebase";
 import { query, collection, onSnapshot, updateDoc, doc, addDoc, deleteDoc } from "firebase/firestore";
 
-import Todo from "./components/Todo/todo";
+import Todo from "./components/todo.js";
 
 
 const style = {
